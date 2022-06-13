1c6c83b0332533fc7359c1883d7c3595c2c0cc36
DESKTOP-DSBGP8J\82107 <kimdohyeong03@naver.com>
2022-06-13 22:43:52 +0900

    Join.java 파일 수정 DB연동 후 회원가입 시 DB 저장

--- src/main/java/join/Join.java
+++ src/main/java/join/Join.java
@@ -1,6 +1,14 @@
 package join;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,21 +24,65 @@ public class Join extends HttpServlet {
 		String id =request.getParameter("id");
 		String pw =request.getParameter("pw");
 		String name =request.getParameter("name");
+		
 		// 전달 받은 값을 하나의 회원 정보로 구성한다 
 		MemberInfo newMemberInfo = new MemberInfo(id,pw,name);
 		
 		// 회원 정보를 DB 에 저장한다 
 		System.out.println("회원 정보를 DB에 저장했음");
-		// 회원 가입 성공 시그널을 보낸다 
-		// 생략 가능
 		
-		boolean joinResult = true;
-		if(joinResult) {
+		Connection conn = null; 
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc.mariadb://localhost:3306/todolist?user=root&password=1234");
+			
+			String sql = "INSERT INTO member_info(memberID,memberPW,name,joinDate) VALUES(?,?,?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, name);
+			pstmt.setString(4, LocalDateTime.now().toString());
+			
+			int count =pstmt.executeUpdate();
+			
+			
 			// 회원 가입 성공 시그널을 보낸다 
-			response.setStatus(HttpServletResponse.SC_OK);
-		}else {
-			response.setStatus(HttpServletResponse.SC_CONFLICT);
+			// 생략 가능
+			
+			boolean joinResult = count ==1 ;
+			if(joinResult) {
+				// 회원 가입 성공 시그널을 보낸다 
+				response.setStatus(HttpServletResponse.SC_OK);
+			}else {
+				response.setStatus(HttpServletResponse.SC_CONFLICT);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if(conn !=null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
+		
+		
+		
+		
 		
 		
 	}
